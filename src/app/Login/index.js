7e399212ef6ca98587c408cda84b45e87c8307cd
yizhiqiang@ly-sky.com <yizhiqiang@ly-sky.com>
2018-07-19 16:55:34 +0800

    创建登录文件

---
+++ src/app/Login/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import { Row , Col} from 'antd';
+
+export default class Login extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
